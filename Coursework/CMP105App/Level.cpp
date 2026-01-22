5ae1b55475d1a6cbf8e870db7666429c9cc15d5f
ConorORe1lly <146092557+ConorORe1lly@users.noreply.github.com>
2026-01-22 14:25:36 +0000

    Added player movement and reset when out of bounds

--- Coursework/CMP105App/Level.cpp
+++ Coursework/CMP105App/Level.cpp
@@ -4,7 +4,9 @@ Level::Level(sf::RenderWindow& hwnd, Input& in) :
 	BaseLevel(hwnd, in)
 {
 	// initialise game objects
-
+	m_snake.setRadius(10);
+	m_snake.setFillColor(sf::Color::Red);
+	m_snake.setPosition({ 350, 350 });
 }
 
 // handle user input
@@ -15,18 +17,65 @@ void Level::handleInput(float dt)
 		std::cout << "left mouse pressed" << std::endl;
 	}
 
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::D))
+	{
+		std::cout << "D pressed" << std::endl;
+		m_direction = MoveDirection::RIGHT;
+	}
+
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::A))
+	{
+		std::cout << "A pressed" << std::endl;
+		m_direction = MoveDirection::LEFT;
+	}
+
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::S))
+	{
+		std::cout << "S pressed" << std::endl;
+		m_direction = MoveDirection::DOWN;
+	}
+
+	if (m_input.isKeyDown(sf::Keyboard::Scancode::W))
+	{
+		std::cout << "W pressed" << std::endl;
+		m_direction = MoveDirection::UP;
+	}
 }
 
 // Update game objects
 void Level::update(float dt)
 {
+	switch (m_direction)
+	{
+	case MoveDirection::UP:
+		m_snake.move({ 0.f, -m_speed * dt });
+		break;
+	case MoveDirection::LEFT:
+		m_snake.move({ -m_speed * dt, 0.f });
+		break;
+	case MoveDirection::RIGHT:
+		m_snake.move({ m_speed * dt, 0.f });
+		break;
+	case MoveDirection::DOWN:
+		m_snake.move({ 0.f, m_speed * dt });
+		break;
+	}
+
+	sf::Vector2u window_size = m_window.getSize();
+
+	sf::Vector2 snake_position = m_snake.getPosition();
+
+	if (snake_position.x > window_size.x || snake_position.y > window_size.y || snake_position.x < 0.f || snake_position.y < 0.f)
+	{
+		m_snake.setPosition({ 300, 300 });
+	}
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
-
+	m_window.draw(m_snake);
 	endDraw();
 }
 
--- Coursework/CMP105App/Level.h
+++ Coursework/CMP105App/Level.h
@@ -15,5 +15,10 @@ private:
 	// Default functions for rendering to the screen.
 
 	// Default variables for level class.
-	
+	sf::CircleShape m_snake;
+
+	float m_speed = 300.f;
+
+	enum class MoveDirection { UP, DOWN, LEFT, RIGHT};
+	MoveDirection m_direction;
 };
